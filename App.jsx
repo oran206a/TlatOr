d8a036fa24d0167635007b996c7c61886684ea67
oran206a <oran206a@gmail.com>
2025-08-21 19:23:08 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{padding:20, fontFamily:"system-ui"}}>
+      <h1>Welcome to TlatOr</h1>
+      <p>This React site is deployed on Netlify from GitHub.</p>
+    </main>
+  )
+}
